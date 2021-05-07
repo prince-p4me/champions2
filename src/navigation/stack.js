07f8dcdf8a594cadc569484f9c811712b786fc7e
Prince Kumar <prince.p4me@gmail.com>
2021-05-07 23:36:43 +0530

    tutorial

--- src/navigation/stack.js
+++ src/navigation/stack.js
@@ -84,7 +84,7 @@ const StackNavigator = () => {
         </>
       ) : (
         <>
-          <Stack.Screen name="Landing" component={TutorialScreen} />
+          <Stack.Screen name="Landing" component={LandingScreen} />
           <Stack.Screen name="SignIn" component={LoginScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />
           <Stack.Screen name="Otp" component={OtpScreen} />
