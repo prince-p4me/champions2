10566ca85729f69d3fd621394d72280ab9ed758e
Prince Kumar <prince.p4me@gmail.com>
2021-05-22 02:04:51 +0530

    Reverted

--- android/app/src/main/java/com/eminence/a10xchampion/MainActivity.java
+++ android/app/src/main/java/com/eminence/a10xchampion/MainActivity.java
@@ -1,4 +1,4 @@
-package com.champions;
+package com.eminence.a10xchampion;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "champions";
+    return "10XChampions";
   }
 }
