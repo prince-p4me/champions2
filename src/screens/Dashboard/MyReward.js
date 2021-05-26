cb03041acb0e0a1d6d37e2336f63b31d8cea1042
himanshukk81 <himanshukk81@gmail.com>
2021-05-26 19:28:44 +0530

    fix crash issue

--- src/screens/Dashboard/MyReward.js
+++ src/screens/Dashboard/MyReward.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ScrollView, FlatList, Image } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, ScrollView, FlatList, Image} from 'react-native';
 import Header from '../../components/Header';
-import { TextRegular, TextThin } from '../../components/TextView';
-import { useSelector, useDispatch } from 'react-redux';
+import {TextRegular, TextThin} from '../../components/TextView';
+import {useSelector, useDispatch} from 'react-redux';
 import i18n from '../../services/i18n';
 
 import RewardPointlayout from '../../components/RewardPointlayout';
@@ -11,7 +11,7 @@ import QRCodeContainer from '../../components/QRCodeContainer';
 import * as Actions from '../../redux/action';
 
 // import { NavigationEvents } from 'react-navigation';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import Color from '../../utility/Color';
 import Constant from '../../utility/Constant';
 import Sizes from '../../utility/Sizes';
@@ -22,69 +22,97 @@ import styles from '../../utility/Style';
 const PointWonLayout = item => {
   const align = isRtl ? 'right' : 'left';
   const isRtl = useSelector(state => state.isRtl);
+  const transactionList = useSelector(state => state.getTransactionByCategory);
 
   let pointInfo = item.item;
-  return (
-    <View style={styles.pointwoncontainer}>
-      <Image
-        source={Images.star3}
-        style={{
-          height: 50,
-          width: 50,
-          alignSelf: 'center',
-          tintColor: Color.darkBGgray,
-        }}></Image>
-      {pointInfo.product_name ?
-        <View style={{ width: "100%", paddingTop: 10, paddingHorizontal: 3 }}>
-          <TextRegular
-            text={'Redeemed ' + pointInfo.points}
-            style={{ textAlign: "center", fontSize: Sizes.semiLarge }}
-          />
-          <TextRegular text={"for " + pointInfo.product_name}
-            style={{ textAlign: "center", fontSize: Sizes.regular, marginTop: 3 }}
-            numberOfLines={2} />
-        </View> :
-        <TextRegular
-          text={'Points Won'}
-          style={{ textAlign: "center", fontSize: Sizes.semiLarge, marginTop: 10 }}
-        />}
-      {!pointInfo.product_name && <View style={{ flexDirection: 'row', marginTop: 5, alignItems: 'center' }}>
+
+  if (transactionList.length > 0 && item?.item) {
+    return (
+      <View style={styles.pointwoncontainer}>
         <Image
-          source={Images.star}
+          source={Images.star3}
           style={{
-            height: 13,
-            width: 13,
+            height: 50,
+            width: 50,
             alignSelf: 'center',
-            marginTop: 3,
-            marginRight: 5,
             tintColor: Color.darkBGgray,
+          }}></Image>
+        {pointInfo.product_name ? (
+          <View style={{width: '100%', paddingTop: 10, paddingHorizontal: 3}}>
+            <TextRegular
+              text={'Redeemed ' + pointInfo.points}
+              style={{textAlign: 'center', fontSize: Sizes.semiLarge}}
+            />
+            <TextRegular
+              text={'for ' + pointInfo.product_name}
+              style={{
+                textAlign: 'center',
+                fontSize: Sizes.regular,
+                marginTop: 3,
+              }}
+              numberOfLines={2}
+            />
+          </View>
+        ) : (
+          <TextRegular
+            text={'Points Won'}
+            style={{
+              textAlign: 'center',
+              fontSize: Sizes.semiLarge,
+              marginTop: 10,
+            }}
+          />
+        )}
+        {!pointInfo.product_name && (
+          <View
+            style={{flexDirection: 'row', marginTop: 5, alignItems: 'center'}}>
+            <Image
+              source={Images.star}
+              style={{
+                height: 13,
+                width: 13,
+                alignSelf: 'center',
+                marginTop: 3,
+                marginRight: 5,
+                tintColor: Color.darkBGgray,
+              }}
+              resizeMode="contain"></Image>
+            <TextRegular
+              text={pointInfo.points}
+              style={{textAlign: align, fontSize: Sizes.regular, marginTop: 5}}
+            />
+          </View>
+        )}
+        <TextThin
+          numberOfLines={3}
+          text={pointInfo.date}
+          style={{
+            textAlign: 'center',
+            fontSize: Sizes.regular,
+            marginTop: 20,
           }}
-          resizeMode="contain"></Image>
-        <TextRegular
-          text={pointInfo.points}
-          style={{ textAlign: align, fontSize: Sizes.regular, marginTop: 5 }}
         />
       </View>
-      }
-      <TextThin
-        numberOfLines={3}
-        text={pointInfo.date}
-        style={{
-          textAlign: "center",
-          fontSize: Sizes.regular,
-          marginTop: 20,
-        }}
-      />
-    </View>
-  );
+    );
+  }
 };
 
 const MyDashboard = () => {
   const [transactionType, setTransactionType] = useState('QR Scan');
   const transactionList = useSelector(state => state.getTransactionByCategory);
+
+  console.log({transactionList: transactionList});
   const user = useSelector(state => state.getUser);
   const dispatch = useDispatch();
 
+  const EmptyListMessage = ({item}) => {
+    return (
+      // Flat List Item
+      // <Text>No Data Found</Text>
+      // <TextRegular></TextRegular>
+      <TextRegular text={'No Data Found'} style={{textAlign: 'center'}} />
+    );
+  };
   function getTransaction(type) {
     let obj = {
       user_id: user.id,
@@ -97,6 +125,7 @@ const MyDashboard = () => {
   }, []);
 
   const ButtonBar = () => {
+    // if (transactionList.length > 0)
     return (
       <View style={styles1.buttons}>
         <TouchableOpacity
@@ -112,7 +141,7 @@ const MyDashboard = () => {
           }}>
           <TextRegular
             text={i18n.t('EarnedWon')}
-            style={{ textAlign: 'center' }}
+            style={{textAlign: 'center'}}
           />
         </TouchableOpacity>
         <TouchableOpacity
@@ -126,33 +155,42 @@ const MyDashboard = () => {
             setTransactionType('Redeemed');
             getTransaction('Redeemed');
           }}>
-          <TextRegular text={i18n.t('redeem')} style={{ textAlign: 'center' }} />
+          <TextRegular text={i18n.t('redeem')} style={{textAlign: 'center'}} />
         </TouchableOpacity>
       </View>
-    )
-  }
+    );
+  };
 
-  console.log({ ListTransaction: transactionList });
+  console.log({ListTransaction: transactionList});
   return (
-    <View style={{ flex: 1, backgroundColor: Color.lightGreen }}>
-      <Header title={I18n.t("rewardpoint")} dashboard={false} back={true} help={true} />
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}
+    <View style={{flex: 1, backgroundColor: Color.lightGreen}}>
+      <Header
+        title={I18n.t('rewardpoint')}
+        dashboard={false}
+        back={true}
+        help={true}
+      />
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1}}
         showsVerticalScrollIndicator={false}>
         <RewardPointlayout />
         <QRCodeContainer bgColor={Color.lightGreen} />
-        <FlatList
-          columnWrapperStyle={{
-            flexGrow: 1,
-            justifyContent: "center",
-          }}
-          data={transactionList}
-          numColumns={2}
-          renderItem={({ item, index }) => <PointWonLayout item={item} />}
-          scrollEnabled={false}
-          keyExtractor={(item, index) => index.toString()}
-          ListFooterComponent={(<View style={{ height: 50 }}></View>)}
-          ListHeaderComponent={(<ButtonBar />)}
-        />
+        <ButtonBar />
+        {transactionList.length > 0 && (
+          <FlatList
+            columnWrapperStyle={{
+              flexGrow: 1,
+              justifyContent: 'center',
+            }}
+            data={transactionList}
+            numColumns={2}
+            renderItem={({item, index}) => <PointWonLayout item={item} />}
+            scrollEnabled={false}
+            keyExtractor={(item, index) => index.toString()}
+            ListFooterComponent={<View style={{height: 50}}></View>}
+            // ListHeaderComponent={<ButtonBar />}
+          />
+        )}
       </ScrollView>
     </View>
   );
